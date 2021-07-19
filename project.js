a61358d3b58778f83970ebccbaf15bb17e13fa0e
rahulcravi9766 <87615703+rahulcravi9766@users.noreply.github.com>
2021-07-19 09:33:05 +0530

    Update project.js

--- project.js
+++ project.js
@@ -1,18 +1,20 @@
+var name1, msg, ph, mail;
+
 function nameValidation() {
   var name = document.getElementById("nametext").value
   var namespan = document.getElementById("namespan");
   var letters = /^[a-zA-Z\s]*$/;
   if (name == "") {
       namespan.innerHTML = "Field is required"
-      return false;
+      name1= false;
   }
   else if (name.match(letters)) {
       namespan.innerHTML = "";
-      return true;
+      name1= true;
   }
   else {
       namespan.innerHTML = "use only characters";
-      return false;
+      name1= false;
   }
 }
 
@@ -22,15 +24,15 @@ function phoneValidation() {
   const letters = /^\d{10,11}$/;
   if (phone == "") {
       phonespan.innerHTML = "Filed is required"
-      return false;
+      ph =false;
   }
   else if (phone.match(letters)) {
       phonespan.innerHTML = "";
-      return true;
+      ph= true;
   }
   else {
       phonespan.innerHTML = "invalid number";
-      return false;
+      ph =false;
   }
 
 }
@@ -41,32 +43,56 @@ function emailValidation() {
   var letters = /^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)$/;
   if (name == "") {
       emailspan.innerHTML = "Filed is required"
-      return false;
+      mail =false;
   }
   else if (name.match(letters)) {
       emailspan.innerHTML = "";
-      return true;
+      mail =true;
   }
   else {
       emailspan.innerHTML = "invalid email";
-      return false;
+      mail= false;
   }
 
 }
 
 function commentValidation() {
   var name = document.getElementById("commenttext").value
-  var messageSpan = document.getElementById("commentspan");
+  var commentspan = document.getElementById("commentspan");
 
   if (name == "") {
       commentspan.innerHTML = "Filed is required"
-      return false;
+      msg= false;
   }
 
 
   else {
       commentspan.innerHTML = "";
-      return true;
+      msg= true;
   }
 
 }
+
+
+    $("#submit-form").submit((e)=>{
+        e.preventDefault()
+        if (msg==true && name1==true && mail==true && ph==true) {
+            
+        
+        $.ajax({
+            url:"https://script.google.com/macros/s/AKfycby9phxiHHy3o5-dbXbZTTEF1zxgsV7_EOMCo-D2TlkWNFKRC8N3q1cCWcKKgYHktuF1Ww/exec",
+            data:$("#submit-form").serialize(),
+            method:"post",
+            success:function (response){
+                alert("Form submitted successfully")
+                window.location.reload()
+                //window.location.href="https://google.com"
+            },
+            error:function (err){
+                alert("Something Error")
+
+            }
+        })
+    }else{alert("Error")
+    }
+    })
