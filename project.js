4abbf920abfc1c17723969276c44e17186103f2e
rahulcravi9766 <87615703+rahulcravi9766@users.noreply.github.com>
2021-07-20 12:21:07 +0530

    Update project.js

--- project.js
+++ project.js
@@ -18,24 +18,49 @@ function nameValidation() {
   }
 }
 
-function phoneValidation() {
-  var phone = document.getElementById("phonetext").value
-  var phonespan= document.getElementById("phonespan");
-  const letters = /^\d{10,11}$/;
-  if (phone == "") {
-      phonespan.innerHTML = "Filed is required"
-      ph =false;
-  }
-  else if (phone.match(letters)) {
-      phonespan.innerHTML = "";
-      ph= true;
-  }
-  else {
-      phonespan.innerHTML = "invalid number";
-      ph =false;
-  }
+function phoneValidation(){
+    var phone=document.getElementById("phonetext").value 
+    var phonespan=document.getElementById("phonespan");
+    var letters= /^\d+$/;
+    
+    if(phone==""){
+        phonespan.innerHTML="Filed is required"
+        return false;}
+    else if(phone.match(letters)&& (phone.length==10)){
+        phonespan.innerHTML="";
+        return true;
+    }
+    else if(phone.length<10)
+        {
+            if(phone.match(letters))
+            {
+                phonespan.innerHTML="Enter minimum 10 numbers";
+                return false;
 
-}
+            }
+            else
+            {
+                phonespan.innerHTML="Use only numbers";
+                return false;
+            }
+
+            
+        }
+    else if(phone.length>10)
+    {
+        if(phone.match(letters))
+        {
+            phonespan.innerHTML="Enter maximum 10 numbers";
+            return false;
+        }
+        else
+        {
+            phonespan.innerHTML="Use only numbers";
+            return false;
+        }
+    
+        
+    }
 
 function emailValidation() {
   var name = document.getElementById("emailaddress").value
